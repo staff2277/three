4ceb137e65259b70d5b29c7f31ce90757a4360b3
Mustapha Osman <wnfynn@gmail.com>
2026-09-11 16:59:29 +0000

    feat: initialize Three.js scene with camera, basic box geometry, and renderer

--- src/main.js
+++ src/main.js
@@ -1,48 +1,25 @@
 import * as THREE from "three";
 
-//SCENE
-let windowWidth = window.innerWidth;
-let windowHeight = window.innerHeight;
 const scene = new THREE.Scene();
 
-//MESH
+const sizes = {
+  windowWidth: window.innerWidth,
+  windowHeight: window.innerHeight,
+  aspectRatio: windowWidth / windowHeight,
+};
 
+//Camera
+const camera = new THREE.PerspectiveCamera(75, sizes.aspectRatio, 1, 200);
+camera.position.z = 3;
+
+//Mesh
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial();
+const boxMaterial = new THREE.MeshBasicMaterial({ color: "green" });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
 scene.add(boxMesh);
 
-//CAMERA
-
-const camera = new THREE.PerspectiveCamera(
-  75,
-  windowWidth / windowHeight,
-  1,
-  1000,
-);
-
-camera.position.z = 3;
-
-//RENDERER
+//Renderer
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(windowWidth, windowHeight);
+renderer.setSize(sizes.windowWidth, sizes.windowHeight);
+renderer.render(scene, camera);
 document.body.appendChild(renderer.domElement);
-
-window.addEventListener("resize", () => {
-  windowWidth = window.innerWidth;
-  windowHeight = window.innerHeight;
-
-  camera.aspect = windowWidth / windowHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(windowWidth, windowHeight);
-});
-
-//ANIMATION
-function animate() {
-  requestAnimationFrame(animate);
-
-  renderer.render(scene, camera);
-}
-
-animate();
