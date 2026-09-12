69aeaba4f4a1489e3e30ca84bb9890117f3d34b8
Mustapha Osman <wnfynn@gmail.com>
2026-09-12 22:20:15 +0000

    Using buffer geometry to create custom geometries

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
@@ -9,41 +10,79 @@ const sizes = {
 let aspectRatio = sizes.windowWidth / sizes.windowHeight;
 
 //Camera
-//const camera = new THREE.PerspectiveCamera(75, aspectRatio, 1, 200);
-const camera = new THREE.OrthographicCamera(
+const camera = new THREE.PerspectiveCamera(75, aspectRatio, 1, 200);
+camera.position.z = 3;
+/* const camera = new THREE.OrthographicCamera(
   -1 * aspectRatio,
   1 * aspectRatio,
   1,
   -1,
   1,
   200,
-);
-camera.position.z = 5;
+); */
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (move) => {
+  cursor.x = (move.clientX / window.innerWidth) * 2 - 1;
+  cursor.y = (move.clientY / window.innerHeight) * -2 + 1;
+});
 
 //Mesh
-const boxGeometry = new THREE.BoxGeometry();
+/* const boxGeometry = new THREE.BoxGeometry();
 const boxMaterial = new THREE.MeshBasicMaterial({ color: "green" });
 const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
-scene.add(boxMesh);
+scene.add(boxMesh); */
+
+//BufferGeometry
+const geometry = new THREE.BufferGeometry();
+const vertexPositions = new Float32Array([
+  0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 0,
+]);
+geometry.setAttribute(
+  "position",
+  new THREE.BufferAttribute(vertexPositions, 3),
+);
+const material = new THREE.MeshBasicMaterial({
+  color: "yellow",
+  wireframe: true,
+});
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
 
 //Renderer
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(sizes.windowWidth, sizes.windowHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+const controls = new OrbitControls(camera, renderer.domElement);
 renderer.render(scene, camera);
+
+function animate(timeStamp) {
+  requestAnimationFrame(animate);
+  controls.update();
+  camera.updateProjectionMatrix();
+  renderer.render(scene, camera);
+}
+animate();
+
+//Events
 window.addEventListener("resize", () => {
   sizes.windowWidth = window.innerWidth;
   sizes.windowHeight = window.innerHeight;
   aspectRatio = sizes.windowWidth / sizes.windowHeight;
-  /* camera.aspect = sizes.windowWidth / sizes.windowHeight; */
-  camera.left = -1 * aspectRatio;
-  camera.right = 1 * aspectRatio;
-  camera.top = 1;
-  camera.bottom = -1;
-
+  camera.aspect = sizes.windowWidth / sizes.windowHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(sizes.windowWidth, sizes.windowHeight);
-  renderer.render(scene, camera);
+});
+let canvas = renderer.domElement;
+window.addEventListener("dblclick", () => {
+  if (document.fullscreenElement === canvas) {
+    document.exitFullscreen();
+  } else {
+    canvas.requestFullscreen();
+  }
 });
 
 document.body.appendChild(renderer.domElement);
